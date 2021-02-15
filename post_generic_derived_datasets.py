ca40f82b3c940256c281a946c7582d0ca328ffaf
Jan Krzysztof Legind <bxq762@science.domain>
2021-02-15 16:34:22 +0100

    rename var.

--- post_generic_derived_datasets.py
+++ post_generic_derived_datasets.py
@@ -9,13 +9,13 @@ def derived_dataset(sourceUrl, title, login, password, api="http://api.gbif-uat.
     '''
     headers = {'Content-Type': 'application/json'}
     now = datetime.datetime.now()
-    inson = {
+    xson = {
               "sourceUrl": sourceUrl,
               "title": title
             }
 
     derivative = requests.post(api ,
-                                   data=json.dumps(inson),
+                                   data=json.dumps(xson),
                                    auth=(login, password),
                                    headers = headers
 
@@ -27,11 +27,11 @@ def derived_dataset(sourceUrl, title, login, password, api="http://api.gbif-uat.
         print("DOI NOT added")
     return derivative
 #
-# relateddatasets = {"6197c830-d9c7-11de-b793-b8a03c50a862": 1,
-#                    "9668b676-f762-11e1-a439-00145eb45e9a": 26,
-#                    "7b7bb606-f762-11e1-a439-00145eb45e9a": 1,
-#                    "50c9509d-22c7-4a22-a47d-8c48425ef4a7": 222}
+relateddatasets = {"6197c830-d9c7-11de-b793-b8a03c50a862": 1,
+                   "9668b676-f762-11e1-a439-00145eb45e9a": 26,
+                   "7b7bb606-f762-11e1-a439-00145eb45e9a": 1,
+                   "50c9509d-22c7-4a22-a47d-8c48425ef4a7": 222}
 
-res = derived_dataset('https://ipt.gbif.org/archive.do?r=jan_test&v=1.5', 'Grapsidae of Spain', 'usrname', 'passw0rd')
+res = derived_dataset('https://ipt.gbif.org/archive.do?r=jan_test&v=1.5', 'Grapsidae of Spain', 'jlegind', 'mussimus')
 
 print(res.content)
